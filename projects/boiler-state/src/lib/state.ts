741de991cfd655dde3db1a397b88f2348c4bab31
Sebastian Post <sebastianpost@online.de>
2023-04-07 18:28:49 +0200

    add documentation

--- projects/boiler-state/src/lib/state.ts
+++ projects/boiler-state/src/lib/state.ts
@@ -4,6 +4,19 @@ import { map } from 'rxjs/operators';
 import { asSelector, logActions } from './helpers';
 import { Selector, SelectorTuple, StateConfig } from './types';
 
+/**
+ * Base class for states. Create an implementation by extending it.
+ * @typeParam S - Interface for the state.
+ * @example
+ * ```ts
+ * interface Office {
+ *  employees: Employee[];
+ *  coffees: Coffee[];
+ * }
+ *
+ * export class OfficeState extends State<Office> { }
+ * ```
+ */
 export abstract class State<S extends Record<string, any>> {
   private readonly _store: BehaviorSubject<S>;
   private readonly _defaults: S;
@@ -20,28 +33,67 @@ export abstract class State<S extends Record<string, any>> {
     }
   }
 
+  /** Returns a Selector of the entire state. */
   public asSelector(): Selector<S> {
     return asSelector(this._store);
   }
 
+  /** Resets the state to the defaults provided in the constructor. */
   public reset(): void {
     this.updateState(() => this._defaults);
   }
 
+  /** Completes the state Observable, unsubscribing all observers and preventing any further emissions. */
   public destroy(): void {
     this._store.complete();
   }
 
+  /** Update the state using {@link https://immerjs.github.io/immer/produce immer} to allow for immutable data and efficient change detection.
+   * @param recipe A function that takes a draft of the current state as a parameter. Perform mutations on this draft to update the state
+   * @example
+   * ```ts
+   * this.updateState((state) => {
+   *  state.coffees.push(new Coffee());
+   *  state.employees = state.employees.filter((employee) => employee.name !== 'John Doe');
+   *
+   *  // update a nested object
+   *  state.employees[5].preferences.theme = 'dark';
+   * });
+   * ```
+   */
   protected updateState(recipe: (currentState: S) => S | void | undefined): void {
     this._store.next(produce(this._store.value, recipe));
     if (this._config.debug) console.log(this.constructor.name, this._store.value);
   }
 
+  /** Selects a slice of the state using by suppling a mapping function.
+   * @param selectorFn A function that takes the current state and returns a slice of the state that you want to select.
+   * @example
+   * ```ts
+   * // select the 'coffees' slice of the state
+   * coffees$ = this.select((state) => state.coffees);
+   * ```
+   * @returns A Selector object that represents the selected slice of the state.
+   */
   protected select<T>(selectorFn: (state: S) => T): Selector<T> {
     const selection = this._store.pipe(map(selectorFn));
     return asSelector(selection);
   }
 
+  /** Combines one or more Selectors and turns them into a new Selector.
+   * @param args An arbitrary amount of Selectors followed by a function that takes the values of the Selectors and returns a new value.
+   * @example
+   * ```ts
+   * // filter black coffees that are liked by any employee
+   * likedBlackCoffees$ = this.derive(this.coffees$, this.employees$, (coffees, employees) =>
+   *  coffees.filter((coffee) => {
+   *    const isLiked = employees.some((employee) => employee.likes(coffee));
+   *    return !coffee.hasMilk && isLiked;
+   *  })
+   * );
+   * ```
+   * @returns A Selector of the supplied function's return value.
+   */
   protected derive<T, Args extends unknown[]>(...args: [...SelectorTuple<Args>, (...args: Args) => T]): Selector<T> {
     const selectorFn = args.at(-1) as (...args: Args) => T;
     const selectors = args.slice(0, args.length - 1) as SelectorTuple<Args>;
--- projects/boiler-state/src/lib/types.ts
+++ projects/boiler-state/src/lib/types.ts
@@ -2,13 +2,13 @@ import { Observable } from 'rxjs';
 
 /** An Observable that is able to retrieve a snapshot of its current value. */
 export type Selector<T> = Observable<T> & {
-  /** An synchronous snapshot of the current observable value. */
+  /** A synchronous snapshot of the current Observable value, retrieved by momentarily subscribing to it. */
   readonly snapshot: T;
   /** Returns a new Selector that uses a custom change comparator.
    * @param changeDefinition
    * * `shallow` - compares the first depth layer of objects and arrays using `===`.
    * * `deep` - compares values by converting them to a JSON string.
-   * * A custom comparator as defined by the `distinctUntilChanged` RxJS operator.
+   * * A custom comparator as defined by the {@link https://rxjs.dev/api/operators/distinctUntilChanged distinctUntilChanged} RxJS operator.
    */
   defineChange(changeDefinition: ChangeDefinition<T>): Selector<T>;
 };

b9bf3f810918f646a42932c61deaf264fdde0775
Sebastian Post <sebastianpost@online.de>
2023-04-08 17:51:24 +0200

    fix asset path for build

--- projects/minesweeper/src/app/pipes/tile-image.pipe.ts
+++ projects/minesweeper/src/app/pipes/tile-image.pipe.ts
@@ -8,20 +8,20 @@ import { GameProcess } from '../models/game-process';
 export class TileImagePipe implements PipeTransform {
   transform(tile: Tile, mouseover: boolean, adjacentMines: number, gameProcess: GameProcess): string {
     if (gameProcess === GameProcess.GameOver) {
-      if (!tile.isMine && tile.isFlagged) return `url('/assets/tile/wrong-flag.png')`;
-      if (tile.isMine && !tile.revealed && !tile.isFlagged) return `url('/assets/tile/mine.png')`;
-      if (tile.isMine && tile.revealed) return `url('/assets/tile/mine-red.jpg')`;
+      if (!tile.isMine && tile.isFlagged) return `url('assets/tile/wrong-flag.png')`;
+      if (tile.isMine && !tile.revealed && !tile.isFlagged) return `url('assets/tile/mine.png')`;
+      if (tile.isMine && tile.revealed) return `url('assets/tile/mine-red.jpg')`;
     }
 
-    if (tile.isFlagged) return `url('/assets/tile/flag.svg')`;
+    if (tile.isFlagged) return `url('assets/tile/flag.svg')`;
 
     if (tile.revealed) {
-      if (adjacentMines) return `url('/assets/tile/${adjacentMines}.svg')`;
-      return `url('/assets/tile/revealed.svg')`;
+      if (adjacentMines) return `url('assets/tile/${adjacentMines}.svg')`;
+      return `url('assets/tile/revealed.svg')`;
     }
 
-    if (mouseover) return `url('/assets/tile/revealed.svg')`;
+    if (mouseover) return `url('assets/tile/revealed.svg')`;
 
-    return `url('/assets/tile/unopened.svg')`;
+    return `url('assets/tile/unopened.svg')`;
   }
 }

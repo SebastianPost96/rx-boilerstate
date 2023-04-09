a8ae2041e80382498a3c2232acc813a8eb69e648
Sebastian Post <sebastianpost@online.de>
2023-04-09 14:17:12 +0200

    exclude minesweeper tests

--- projects/minesweeper/src/app/pipes/tile-image.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TileImagePipe } from './tile-image.pipe';
-
-describe('TileclassPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TileImagePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
